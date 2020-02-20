f0609dca61816ec5efb8a88a15bada6e8653855d
Shashi Nag <s.nag@razorcat.com>
2020-02-20 10:01:16 +0100

    modified:   Sources/Is_Value_In_Range.c : v1->V1 and r1->R1

--- Sources/Is_Value_In_Range.c
+++ Sources/Is_Value_In_Range.c
@@ -11,22 +11,22 @@ typedef int value;
 
 typedef enum {no, yes} result;
 
-// Checks if r1.start <= v1 < (r1.start + r1.len),
-// e.g. r1.start = 5, r1.len = 2
-// ==> v1 == 4 ---> no
-//     v1 == 5 ---> yes
-//     v1 == 6 ---> yes
-//     v1 == 7 ---> no
+// Checks if R1.start <= V1 < (R1.start + R1.len),
+// e.g. R1.start = 5, R1.len = 2
+// ==> V1 == 4 ---> no
+//     V1 == 5 ---> yes
+//     V1 == 6 ---> yes
+//     V1 == 7 ---> no
 // However, the implementation is intentionally
-// erroneous: v1 == 7 results "yes" instead of "no"!
+// erroneous: V1 == 7 results "yes" instead of "no"!
 //
 
-result is_value_in_range (struct range r1, value v1)
+result is_value_in_range (struct range R1, value V1)
 {
-	if (v1 < r1.range_start)
+	if (V1 < R1.range_start)
 		return no;
 
-	if (v1 > (r1.range_start + r1.range_len))
+	if (V1 > (R1.range_start + R1.range_len))
 		return no;
 
 	return yes;
